77aa0191feb079228e0c76e99a3dfd8191c47866
MayankVaswani100 <coder.mayan@gmail.com>
2021-05-27 22:12:28 +0530

    Add Operators, String Methods, Useful functions

--- main.py
+++ main.py
@@ -14,10 +14,41 @@ var321 = "344.444"
 string1 = "Hello World!!"
 string2 = '11.3'
 ## Float
-float = 23.0
+float1 = 23.0
 ## Int
 integer1 = -2
 integer2 = 5
 ## Bool
 boolean1 = True
 boolean2 = False
+
+# Some useful functions
+input1 = input("Name: ") ## Getting User Input
+ord1 = ord("Z") ## Show the ascii code of a character
+
+# Arithmetic Operators
+multiplication = 1*3
+division = 2/23567
+addition = 1+2
+subtraction = 5-333
+exponent = 22**22
+percent = 22 % 9
+
+# String methods
+hello0 = "hello".upper() # Uppercases the string
+hello1 = "hello".upper() # Lowercases the string
+hello2 = "hello".capitalize() # Capitalize the string
+hello3 = "hello".count("ll") # Count the number of things in the string
+hello4 = hello3 + hello1
+hello5 = hello4 * 4
+
+# Conditional Opeators
+equality1 = "hello"
+equality2 = "hello1"
+equality = 92 == 2
+print(equality1 == equality2) # Check if both things are same
+
+noequality1 = "hello"
+noequality2 = "hello1"
+print(equality1 != equality2) # Check if both things are not the same
+
