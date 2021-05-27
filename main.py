8db2a6942fe5a35006b5656a689cebb4088b26a9
MayankVaswani100 <coder.mayan@gmail.com>
2021-05-27 11:05:38 +0530

    Add Print, Variables, Data Types, Comments

---
+++ main.py
@@ -0,0 +1,23 @@
+# This is a comment. It is ignored by the interpreter
+
+# Printing
+print("Hola me amigos")
+print("I am mayank", 13)
+
+# Variables
+var = 1
+var321 = "344.444"
+
+# Data Types
+
+## String
+string1 = "Hello World!!"
+string2 = '11.3'
+## Float
+float = 23.0
+## Int
+integer1 = -2
+integer2 = 5
+## Bool
+boolean1 = True
+boolean2 = False
